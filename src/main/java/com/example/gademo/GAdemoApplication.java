243c50ea7754303adcbb6c7dd54a688b129c865f
vulpyalt <vulpyalt@gmail.com>
2022-08-23 09:24:06 +0200

    first

---
+++ src/main/java/com/example/gademo/GAdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GAdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GAdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gademo/GAdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GAdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
